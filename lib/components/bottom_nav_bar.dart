e59e79c1a204f4900b7bf394bebb3d01867bdbf0
bilalshahid929 <bilalshahid929@gmail.com>
2025-01-09 07:00:18 +0500

    feat: add the initial boilerplate screens

---
+++ lib/components/bottom_nav_bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class MyBottomNavBar extends StatelessWidget {
+  final void Function(int)? onTabChange;
+  const MyBottomNavBar({super.key, required this.onTabChange});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 20.0),
+      child: GNav(
+        color: Colors.grey.shade400,
+        activeColor: Colors.grey.shade800,
+        tabActiveBorder: Border.all(color: Colors.white),
+        tabBackgroundColor: Colors.grey.shade100,
+        mainAxisAlignment: MainAxisAlignment.center,
+        tabBorderRadius: 16,
+        onTabChange: (value) => onTabChange!(value),
+        tabs: const [
+          GButton(icon: Icons.home, text: 'shop'),
+          GButton(
+            icon: Icons.shopping_bag_rounded,
+            text: 'Cart',
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,66 @@
+import 'package:ecommerce_app/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.grey,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(
+                  'lib/images/nike.png',
+                  height: 350,
+                ),
+              ),
+              const Text("Just Do It",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22)),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Brand new sneakers and custom kicks made with premium quality",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              GestureDetector(
+                onTap: () => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const HomePage())),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade900,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: const EdgeInsets.all(25),
+                  child: const Center(
+                    child: Text(
+                      "Shop Now",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
